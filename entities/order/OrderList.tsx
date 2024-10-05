393cc36f22540283b98173797c2cccf85313bf8d
Nata <nuts08air@inbox.ru>
2024-10-05 22:41:08 +0800

    корректировка стилей

--- entities/order/OrderList.tsx
+++ entities/order/OrderList.tsx
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 16,
     color: Colors.darkText,
-    marginBottom: 4,
+    marginBottom: 2,
   },
   orderDescr: {
     fontFamily: Fonts.regular,
